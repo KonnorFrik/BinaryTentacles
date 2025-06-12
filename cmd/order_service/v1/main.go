a84aa219dceae43142a1555ae6eba57aa60e7eca
Konnor <konnor.frik666@gmail.com>
2025-06-12 20:51:08 +0500

    main:/cmd/order_service/v1: add base file

---
+++ cmd/order_service/v1/main.go
@@ -0,0 +1,77 @@
+/*
+Simple gRPC user_auth server implemented user_auth/v1
+*/
+package main
+
+import (
+	"context"
+	"errors"
+	"net"
+
+
+    "github.com/KonnorFrik/BinaryTentacles/pkg/logging"
+    pb "github.com/KonnorFrik/BinaryTentacles/internal/generated/order_service/v1"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+)
+
+type server struct {
+	pb.UnimplementedOrderServiceServer
+}
+
+var (
+	logger = logging.New()
+)
+
+const (
+	laddr = ":9999"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", laddr)
+
+	if err != nil {
+		logger.Error("[Server/Listen]", "error", err)
+		return
+	}
+
+	userServer := &server{}
+	grpcServer := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			logger.UnaryServerInterceptor,
+		),
+	)
+	pb.RegisterOrderServiceServer(grpcServer, userServer)
+	logger.Info("Listen at", "local address", laddr)
+
+	err = grpcServer.Serve(listener)
+
+	if err != nil {
+		logger.Error("Serve", "error", err)
+		return
+	}
+}
+
+// WrapError - wrap usecase error into gRPC error with codes
+func WrapError(err error) error {
+	var code = codes.Internal
+	var msg string
+	switch {
+	// case errors.Is(err, usecase.ErrDoesNotExist):
+	// 	code = codes.NotFound
+	// case errors.Is(err, usecase.ErrAlreadyExist):
+	// 	code = codes.AlreadyExists
+	// case errors.Is(err, usecase.ErrInvalidData):
+	// 	code = codes.InvalidArgument
+	// case errors.Is(err, usecase.ErrDbNoAccess):
+	// 	// default = Internal
+	// case errors.Is(err, usecase.ErrUnknown):
+		// default = Internal
+	}
+
+	return status.Error(code, msg)
+}
+
